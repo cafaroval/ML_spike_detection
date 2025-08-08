4ea4e976d2f5dce96c342706317117c30da2f875
cafaroval <143494976+cafaroval@users.noreply.github.com>
2025-08-08 14:12:12 +0200

    Create create_epochs.m

---
+++ scripts/create_epochs.m
@@ -0,0 +1,23 @@
+% This script performs:
+% Epoching of raw EEG data
+
+fs = 200;
+epoch_length = 1;
+samples_per_epoch = fs * epoch_length;
+total_hour = 22;
+
+%% Segment Raw EEG into 1-Second Epochs
+epochedEEG = cell(1, total_hour);
+for hour = 1:total_hour
+    currentEEG = eegData{hour};
+    [numChannels, totalSamples] = size(currentEEG);
+    numEpochs = floor(totalSamples / samples_per_epoch);
+    epochedData = zeros(numChannels, samples_per_epoch, numEpochs);
+
+    for epochIdx = 1:numEpochs
+        startIdx = (epochIdx - 1) * samples_per_epoch + 1;
+        endIdx = startIdx + samples_per_epoch - 1;
+        epochedData(:, :, epochIdx) = currentEEG(:, startIdx:endIdx);
+    end
+    epochedEEG{hour} = epochedData;
+end
