2a88a0e3cf1f93048fc603f89564d97730a154c9
cafaroval <143494976+cafaroval@users.noreply.github.com>
2025-08-08 18:58:51 +0200

    Create preprocessing.m

---
+++ script/preprocessing.m
@@ -0,0 +1,16 @@
+%% Preprocess Spike and Non-Spike Epochs ==========
+channels_to_remove = [18, 21:31];
+channels_to_keep = setdiff(1:31, channels_to_remove);
+eeg_labels = {'Fp1', 'Fp2', 'F3', 'F4', 'C3', 'C4', 'P3', 'P4', 'O1', 'O2', ...
+              'F7', 'F8', 'T3', 'T4', 'T5', 'T6', 'Fz', 'Cz', 'Pz'};
+epochs = AndSpike_eeg; %done
+AndSpike_eeg_clean = preprocess_epochs(AndSpike_eeg, fs, channels_to_keep, eeg_labels);
+
+epochs = AndNonSpike_eeg; %done
+AndNonSpike_eeg_clean = preprocess_epochs(AndNonSpike_eeg, fs, channels_to_keep, eeg_labels);
+
+epochs = OrSpike_eeg; %done
+OrSpike_eeg_clean = preprocess_epochs(OrSpike_eeg, fs, channels_to_keep, eeg_labels);
+
+epochs = OrNonSpike_eeg; %done
+OrNonSpike_eeg_clean = preprocess_epochs(OrNonSpike_eeg, fs, channels_to_keep, eeg_labels);
